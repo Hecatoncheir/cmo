26ae370a7426bfa6e308ed5e5708744af32b28f3
Vitaliy Vostrikov <rasart.pro@gmail.com>
2014-12-16 11:20:44 +0300

    added section_menu.js

--- web/scripts/section_menu.js
+++ web/scripts/section_menu.js
@@ -1,20 +1,9 @@
-  $(document).ready(function() {
-    var tags = $('.section li a');
-    tags.on('click', function() {
-      $(this).removeAttr('href');
-      var element = $(this).parent('li');
-      if (element.hasClass('open')) {
-        element.removeClass('open');
-        element.find('li').removeClass('open');
-        element.find('ul').slideUp();
-      } else {
-        element.addClass('open');
-        element.children('ul').slideDown();
-        element.siblings('li').children('ul').slideUp();
-        element.siblings('li').removeClass('open');
-        element.siblings('li').find('li').removeClass('open');
-        element.siblings('li').find('ul').slideUp();
-      }
+    $(document).ready(function() {
+      var elementsList = $('.sub-section');
+      elementsList.each(function(index){
+        $(this).children('ul').slideUp('fast');
+        $(this).click(function () {
+          $(this).children('ul').slideToggle('fast');
+        })
+      });
     });
-  });
-
